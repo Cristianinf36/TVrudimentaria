868fbb80cb85f66d621b047b2624ab0c55b468fd
Cristian <cristianinf36@gmail.com>
2019-04-07 20:19:32 -0400

    Finalizado 07/04/2019

--- CristianTV/Form1.Designer.cs
+++ CristianTV/Form1.Designer.cs
@@ -28,13 +28,302 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.txtCanal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Cambia = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Borrar = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.On = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(597, 426);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            // 
+            // txtCanal
+            // 
+            this.txtCanal.Location = new System.Drawing.Point(16, 65);
+            this.txtCanal.Name = "txtCanal";
+            this.txtCanal.Size = new System.Drawing.Size(23, 20);
+            this.txtCanal.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Canal";
+            // 
+            // Cambia
+            // 
+            this.Cambia.Location = new System.Drawing.Point(86, 65);
+            this.Cambia.Name = "Cambia";
+            this.Cambia.Size = new System.Drawing.Size(75, 23);
+            this.Cambia.TabIndex = 3;
+            this.Cambia.Text = "Cambiar";
+            this.Cambia.UseVisualStyleBackColor = true;
+            this.Cambia.Click += new System.EventHandler(this.Cambia_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(16, 94);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(36, 23);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(58, 94);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(36, 23);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(100, 94);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(36, 23);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(16, 123);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(36, 23);
+            this.btn4.TabIndex = 7;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(58, 123);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(36, 23);
+            this.btn5.TabIndex = 8;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(100, 123);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(36, 23);
+            this.btn6.TabIndex = 9;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(16, 152);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(36, 23);
+            this.btn7.TabIndex = 10;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(58, 152);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(36, 23);
+            this.btn8.TabIndex = 11;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(100, 152);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(36, 23);
+            this.btn9.TabIndex = 12;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(16, 180);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(36, 23);
+            this.btn0.TabIndex = 13;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.Borrar);
+            this.groupBox1.Controls.Add(this.Next);
+            this.groupBox1.Controls.Add(this.Back);
+            this.groupBox1.Controls.Add(this.On);
+            this.groupBox1.Controls.Add(this.txtCanal);
+            this.groupBox1.Controls.Add(this.btn0);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn9);
+            this.groupBox1.Controls.Add(this.Cambia);
+            this.groupBox1.Controls.Add(this.btn8);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.Controls.Add(this.btn7);
+            this.groupBox1.Controls.Add(this.btn2);
+            this.groupBox1.Controls.Add(this.btn6);
+            this.groupBox1.Controls.Add(this.btn3);
+            this.groupBox1.Controls.Add(this.btn5);
+            this.groupBox1.Controls.Add(this.btn4);
+            this.groupBox1.Location = new System.Drawing.Point(615, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 209);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control";
+            // 
+            // Borrar
+            // 
+            this.Borrar.Location = new System.Drawing.Point(61, 181);
+            this.Borrar.Name = "Borrar";
+            this.Borrar.Size = new System.Drawing.Size(75, 23);
+            this.Borrar.TabIndex = 19;
+            this.Borrar.Text = "Borrar";
+            this.Borrar.UseVisualStyleBackColor = true;
+            this.Borrar.Click += new System.EventHandler(this.Borrar_Click);
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(120, 19);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(46, 37);
+            this.Next.TabIndex = 18;
+            this.Next.Text = ">>";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(68, 19);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(46, 37);
+            this.Back.TabIndex = 17;
+            this.Back.Text = "<<";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // On
+            // 
+            this.On.BackgroundImage = global::CristianTV.Properties.Resources.shutdown_off_close_exit_15253;
+            this.On.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.On.Location = new System.Drawing.Point(16, 19);
+            this.On.Name = "On";
+            this.On.Size = new System.Drawing.Size(46, 37);
+            this.On.TabIndex = 16;
+            this.On.UseVisualStyleBackColor = true;
+            this.On.Click += new System.EventHandler(this.On_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(615, 227);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(173, 211);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Guía";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(7, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(160, 186);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Cristian TV";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.TextBox txtCanal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Cambia;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button On;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Borrar;
     }
 }
 
--- CristianTV/Form1.cs
+++ CristianTV/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace CristianTV
@@ -15,6 +16,235 @@ namespace CristianTV
         public Form1()
         {
             InitializeComponent();
+            //Para evitar que el usuario cambie la vista a la ventana
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            //Variable de encendido o apagado
+            power = true;
+            //Array bidimensional de canales.
+            canales = new string[,]{
+                {"2","https://ss1.domint.net:3126/ta2_str/teleantillas/playlist.m3u8" },
+                {"5","https://playback.akamaized.net/streams/28126860_8531341_lsi1pcducrzz1i7nf83_1/media/28126860_8531341_lsi1pcducrzz1i7nf83_1@446000pb.m3u8?dw=80&ts=1554670800&hdnts=exp=1554845897~acl=/streams/28126860_8531341_lsi1pcducrzz1i7nf83_1/media*~hmac=c33a88843933b175dde299277cdfeb9bdb518b42ddbdd74d88bd0bf0874162f6"} ,
+                {"9","https://stream-02.nyc.dailymotion.com/sec(d6Li0alGMG4oHIKxMrrxpg92VpQkmWBM78_15VIpbds)/dm/3/x6tt2y8/live.m3u8#cell=lnyc&comment=TRS&qos_vpart=1&qos_stail=1"} ,
+                {"11","https://ss3.domint.net:3114/t11_str/telesistema/playlist.m3u8"} , 
+                {"13","https://playback.akamaized.net/streams/28126860_8555433_lsil4l41wxb1ravw87q_1/media/28126860_8555433_lsil4l41wxb1ravw87q_1@696000pb.m3u8?dw=80&ts=1554678000&hdnts=exp=1554852106~acl=/streams/28126860_8555433_lsil4l41wxb1ravw87q_1/media*~hmac=cc44c68d5222ab6994778a13b273876072130c1a23aad9b86a0b401d412e05d3"} ,
+                {"15","https://playback.akamaized.net/streams/28126860_8555544_lsiwcec8oizvke1n04w_1/media/28126860_8555544_lsiwcec8oizvke1n04w_1@446000p.m3u8?dw=80&ts=1554670800&hdnts=exp=1554844599~acl=/streams/28126860_8555544_lsiwcec8oizvke1n04w_1/media*~hmac=852cdb5f65d52d3e11257cbd6d365dcc330f6e3854ef5c9432f7988f74423367"} ,
+                {"19","http://cinevision01.streamprolive.com/hls/live.m3u8" } ,
+                {"23","http://ss8.domint.net:2118/tf_str/futu/playlist.m3u8"} ,
+                {"29","https://ss1.domint.net:3302/tu_str/universo/chunklist_w1317328852.m3u8"} 
+                
+            };
+
+            //URL inicial
+            urlCanal = canales[0, 1];
+            axWindowsMediaPlayer1.URL = urlCanal;
+            txtCanal.Text = canales[0,0];
+
+            //Rellenar el listbox
+            RenellenaListBox();
         }
+        
+
+        //Método para que continue la reproducción
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender,
+                AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 10)
+            {
+                
+                    this.BeginInvoke(new Action(() => {
+                        if (power != false)
+                        {
+                            this.axWindowsMediaPlayer1.URL = urlCanal;
+                        }
+                    }));
+                
+                
+            }
+        }
+        
+        
+        //Método para encender y apagar.
+        private void On_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                axWindowsMediaPlayer1.URL = "";
+                power = false;
+                label1.Enabled = false;
+                txtCanal.Enabled = false;
+                Back.Enabled = false;
+                Next.Enabled = false;
+                Cambia.Enabled = false;
+                btn0.Enabled = false;
+                btn1.Enabled = false;
+                btn2.Enabled = false;
+                btn3.Enabled = false;
+                btn4.Enabled = false;
+                btn5.Enabled = false;
+                btn6.Enabled = false;
+                btn7.Enabled = false;
+                btn8.Enabled = false;
+                btn9.Enabled = false;
+                Borrar.Enabled = false;
+                listBox1.Enabled = false;
+            }
+            else
+            {
+                axWindowsMediaPlayer1.URL = urlCanal;
+                power = true;
+                label1.Enabled = true;
+                txtCanal.Enabled = true;
+                Back.Enabled = true;
+                Next.Enabled = true;
+                Cambia.Enabled = true;
+                btn0.Enabled = true;
+                btn1.Enabled = true;
+                btn2.Enabled = true;
+                btn3.Enabled = true;
+                btn4.Enabled = true;
+                btn5.Enabled = true;
+                btn6.Enabled = true;
+                btn7.Enabled = true;
+                btn8.Enabled = true;
+                btn9.Enabled = true;
+                Borrar.Enabled = true;
+                listBox1.Enabled = true;
+            }
+        }
+
+        
+        //Botones numericos
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "9";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text += "0";
+        }
+
+        private void Borrar_Click(object sender, EventArgs e)
+        {
+            txtCanal.Text = "";
+        }
+        
+
+        //Botón de cambiar
+        private void Cambia_Click(object sender, EventArgs e)
+        {
+            bool isFound = false;
+            for(int i = 0; i< 9; i++)
+            {
+                if (txtCanal.Text.Equals(canales[i, 0]))
+                {
+                    urlCanal = canales[i, 1];
+                    this.axWindowsMediaPlayer1.URL = urlCanal;
+                    isFound = true;
+                }
+            }
+            if (isFound == false)
+            {
+                MessageBox.Show("Canal no disponible. Disculpe los inconvenientes");
+            }
+            
+        }
+
+        //Botón de ir hacia atrás
+        private void Back_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if ( urlCanal.Equals(canales[i, 1]) && i != 0)
+                {
+                    urlCanal = canales[i-1, 1];
+                    this.axWindowsMediaPlayer1.URL = urlCanal;
+                    txtCanal.Text = canales[i - 1, 0];
+                }
+            }
+        }
+        //Botón hacia adelante
+        private void Next_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (urlCanal.Equals(canales[i, 1]) && i != 8)
+                {
+                    urlCanal = canales[i + 1, 1];
+                    this.axWindowsMediaPlayer1.URL = urlCanal;
+                    txtCanal.Text = canales[i + 1, 0];
+                    break;
+                }
+            }
+        }
+        //Método para rellenar la lista
+        private void RenellenaListBox()
+        {
+            
+            listBox1.Items.Add("Canal 2: Teleantillas");
+            listBox1.Items.Add("Canal 5: Telemicro");
+            listBox1.Items.Add("Canal 9: Color Visión");
+            listBox1.Items.Add("Canal 11: Telesistema");
+            listBox1.Items.Add("Canal 13: Telecentro");
+            listBox1.Items.Add("Canal 15: Digital 15");
+            listBox1.Items.Add("Canal 19: Cinevision");
+            listBox1.Items.Add("Canal 23: Telefuturo");
+            listBox1.Items.Add("Canal 29: Teleuniverso");
+            
+        }
+
+        //Cambiar de canal a través del listbox
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            urlCanal = canales[listBox1.SelectedIndex, 1];
+            axWindowsMediaPlayer1.URL = urlCanal;
+            txtCanal.Text = canales[listBox1.SelectedIndex, 0];
+        }
+
+        //Variables
+        private bool power;
+        private string[,] canales;
+        private string urlCanal;
     }
 }
